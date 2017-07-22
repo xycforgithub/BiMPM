d1e0e0dab40733a0f6adf24ab38ddacba581abea
Yichong Xu <t-yicxu@microsoft.com>
2017-07-21 22:21:53 -0700

    rl first

--- src/my_rnn.py
+++ src/my_rnn.py
@@ -18,6 +18,9 @@ import tensorflow as tf
 
 _state_size_with_prefix = rnn_cell_impl._state_size_with_prefix
 
+def concatenate_sents(sent1_repre, sent2_repre, sent1_lengths, sent2_lengths)
+
+
 def _dynamic_rnn_loop(cell, inputs, initial_state, parallel_iterations, swap_memory, sequence_length=None, dtype=None):
   """Internal implementation of Dynamic RNN.
 
